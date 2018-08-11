76b9982b5bb91f6f7fc1b36a4d0594956fe4ef8d
Joshua Crowe <joshuacrowe@outlook.com>
2018-08-11 09:59:37 +0100

    Page Component
    Added a page component to encapsulate the different panels.

---
+++ public/components/Page.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="page">
+        <div id="page-topshelf" class="row">
+            <div class="col s4 offset-s2">
+                <ul id="tabs" class="tabs grey darken-4">
+                    <li class="tab col s3"><a href="#test1">Queue</a></li>
+                    <li class="tab col s4"><a href="#test2">History</a></li>
+                    <li class="tab col s3"><a href="#test3">Device</a></li>
+                </ul>
+            </div>
+        </div>
+        <div id="page-content" class="row">
+            <div id="test1" class="col s12">Test 1</div>
+            <div id="test2" class="col s12">Test 2</div>
+            <div id="test3" class="col s12">Test 3</div>
+            <div id="test4" class="col s12">Test 4</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    module.exports = {}
+</script>
