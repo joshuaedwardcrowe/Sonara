455174a3002734c0232a8c5c097d049a7f82356d
Joshua Crowe <joshuacrowe@outlook.com>
2018-08-18 00:28:48 +0100

    Finished Overview Component
    Overview component to match queue song component.

--- public/components/song/SongOverview.vue
+++ public/components/song/SongOverview.vue
@@ -2,14 +2,27 @@
     <div id="overview" class="card">
         <div class="card-image">
             <img src="images/album.jpg">
-            <span class="card-title">Card Title</span>
+            <span class="card-title">Drake - Hotline Bling</span>
         </div>
         <div class="card-content">
-            <p>I am a very simple card. I am good at containing small bits of information.
-                I am convenient because I require little markup to use effectively.</p>
-        </div>
-        <div class="card-action">
-            <a href="#">This is a link</a>
+            <div class="progress grey">
+                <div class="determinate grey darken-4" style="width: 70%"></div>
+            </div>
+            <div class="row">
+                <div class="col s2">0:22</div>
+                <div class="col s2 offset-s8">3:49</div>
+            </div>
+            <div class="row">
+                <div class="col s12 center-align">
+                    <a class="waves-effect waves-light btn red darken-2"><i class="material-icons left">arrow_downward</i>22,000</a>
+                    <a class="waves-effect waves-light btn green darken-2"><i class="material-icons right">arrow_upward</i>22,000</a>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col s12 center-align">
+                    <p class="bottom">&copy; Something Records 2018</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
