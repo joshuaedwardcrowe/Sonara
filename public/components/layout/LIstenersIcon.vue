285452c48644a26fc18c4ed0ad345548e7b8d665
Joshua Crowe <joshuacrowe@outlook.com>
2018-08-18 17:57:52 +0100

    Listeners Icon
    Moved listener count icon to component.

---
+++ public/components/layout/LIstenersIcon.vue
@@ -0,0 +1,17 @@
+<template>
+    <li>
+        <a>
+            <i class="material-icons right">hearing</i>
+            {{count}} Listening
+        </a>
+    </li>
+</template>
+
+<script>
+    module.exports = {
+      props: {count: Number},
+      mounted: function () {
+        console.log("Listeners Icon component mounted.");
+      }
+    }
+</script>
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -6,11 +6,16 @@ const Twas = require('twas')
 Vuegister.register();
 
 const BarComponent = require('./components/layout/Bar.vue');
-const ConnectionIconCOmponent = require('./components/layout/ConnectionIcon.vue');
+const ConnectionIconComponent = require('./components/layout/ConnectionIcon.vue');
+const ListenersIconComponent = require('./components/layout/ListenersIcon.vue');
 
 window.SonaraApp = new Vue({
   el: '#sonara-app',
-  components: {Bar: BarComponent, ConnectionIcon: ConnectionIconCOmponent},
+  components: {
+    Bar: BarComponent, 
+    ConnectionIcon: ConnectionIconComponent, 
+    ListenersIcon: ListenersIconComponent
+  },
   mounted: function () {
     console.log(`Vue mounted.`)
   },
