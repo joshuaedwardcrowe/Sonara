ab251b06c83702302f7475c535ec069101091a37
Joshua Crowe <joshuacrowe@outlook.com>
2018-08-19 18:19:25 +0100

    Generic List Component
    List component for CollctionItems.

---
+++ public/components/shared/List.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul class="collection">
+        <slot></slot>
+    </ul>
+</template>
+
+<script>
+    module.exports = {
+      mounted: function () {
+        console.log("List component mounted.")
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- public/components/user/NotificationList.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <ul class="collection">
-        <slot></slot>
-    </ul>
-</template>
-
-<script>
-    module.exports = {}
-</script>
